42af7b9c62cc6f24e5c8b5780cc684a15ee84fdf
Tuong Tran <tranvantuong2k3@gmail.com>
2025-05-15 18:13:59 +0700

    prepare-deploy-render

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routers import healthcheck, bin_status, control_bin, predict, predict_iot
 from config.swagger import custom_openapi
+import uvicorn
+import os
 
 app = FastAPI(
     title="Waste Classification API",
@@ -11,7 +13,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Vite localhost
+    allow_origins=["http://localhost:5173"],  # Update for production later
+    # allow_origins=["https://waste-classification-blush.vercel.app"]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -24,5 +27,9 @@ app.include_router(control_bin.router)
 app.include_router(predict.router)
 app.include_router(predict_iot.router)
 
-# Set custom OpenAPI schema using a lambda to pass the app instance
+# Set custom OpenAPI schema
 app.openapi = lambda: custom_openapi(app)
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))  # Use Render's PORT or default to 8000
+    uvicorn.run(app, host="0.0.0.0", port=port)
